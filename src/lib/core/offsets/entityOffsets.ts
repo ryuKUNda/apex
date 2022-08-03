8b979af2e92c633966beba89b7b60a9bef0dacba
ryuKUNda <67267826+ryuKUNda@users.noreply.github.com>
2022-08-04 08:27:55 +0900

    Add files via upload

--- src/lib/core/offsets/entityOffsets.ts
+++ src/lib/core/offsets/entityOffsets.ts
@@ -3,7 +3,7 @@ export const entityOffsets = {
   iTeamNum: Number('0x448'),
   iName: Number('0x589'),
   m_thirdPersonShoulderView: Number('0x36a8'),
-  m_thirdPerson: Number('base_address + 0x01a02db0'),
+  m_thirdPerson: Number('0x01a04db0 + 0x6c'),
   glowEnable: Number('0x3c8'),
   glowThroughWall: Number('0x3d0')
 };
